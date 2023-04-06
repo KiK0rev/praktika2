8b23d3c784c8fd82018be7b37015369b2b3eff0e
Kozerev <dly.youtuba001@gmail.com>
2023-04-06 09:34:10 +0300

    Second commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// let name = "Kirill";
+// console.log (`Привет, ${name}!`);
+
+// function isEven (a) {
+//     if(a%2 == 0){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(isEven(14));
+
+// let person = {
+//     name: "Кирилл",
+//     age: 18,
+//     city: "Великий Новгород",
+// };
+
+// function myName(person) {
+//     return ("Меня зовут " + person.name + ". Мне " + person.age + " лет. Я живу в городе " + person.city + ".");
+// };
+
+// console.log(myName(person));
+
+// let arr = [1, 2, 3, 4, 10]
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// };
+ 
+// console.log(sum);
+
+let car = {
+    make: "FORD",
+    model: "MUSTANG",
+    year: 2016,
+};
+
+function myCar(car) {
+    return ("Я езжу на " + car.make + " " + car.model + ", выпущенную в " + car.year + " году.");
+};
+
+console.log(myCar(car));
